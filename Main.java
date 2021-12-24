1c28d1e4a073d81add9dba3b259dcf7eb23cda03
larsyx <64844366+larsyx@users.noreply.github.com>
2021-12-24 21:38:58 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame frame=new JFrame();
+		frame.setSize(1900,1080);
+		frame.setTitle("Forza 4");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+		
+
+	}
+
+}
